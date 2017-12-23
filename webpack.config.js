176927a7cd13b5489e192579e4960219c297f68f
Ranga Senal Fernando <ranga.fernando@gmail.com>
2017-12-23 18:24:54 +1100

    configure with webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path:__dirname + "/dist"
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extentions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules : [
+            // All files with a '.ts' or '.tsx' estension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            // All output '.js' files will have any sourcemap re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }    
+        ]   
+    },
+
+    externals: {
+        "react" : "React",
+        "react-dom": "ReactDOM"
+    },
+}
